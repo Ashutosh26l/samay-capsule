2d96a8b07e567db6e98783d1240579d900fefef8
ashutosh26l <aksmba2611@gmail.com>
2025-07-22 20:58:52 +0530

    fixed vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ Required for Vercel if using dist
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
